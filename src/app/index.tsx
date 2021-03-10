5710338c88e7c981154fca9b933c92cfe47251ca
unknown <allanwallace.aw@gmail.comgit config --global user.name devallanwgit commit -m first>
2021-03-09 21:27:47 -0300

    first commit

---
+++ src/app/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './styles.css'
+
+const App = () => {
+  return (
+    <div className="container">
+      <div>
+        <h1>WILSON é o melior singaro!</h1>
+      </div>
+      <div>
+        E tenho dito!
+      </div>
+      <div id="janjao">
+        Janjão mama traveco!
+      </div>
+    </div>
+  );
+}
+
+export default App;
